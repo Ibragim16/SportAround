4da435e0ed6226314bc40f7d51912289674cfd01
Ibragim <dragunov.lion@gmail.com>
2022-09-15 18:54:07 +0300

    Исправил некоторые неточности и оптимизировал код

--- src/App.jsx
+++ src/App.jsx
@@ -4,47 +4,63 @@ import Event from "./components/EventPage/EventPage";
 import TimePage from "./components/TimePage/TimePage";
 
 function App() {
-  const [state, setState] = useState([]);
+  let array = [
+    {
+      "title": "Международный шахматный форум \"Moscow Open\"",
+      "is_main": false,
+      "dt_start": "2022-11-14T16:00:00+03:00",
+      "dt_end": "2022-11-20T20:00:00+03:00",
+      "dt_create": "2022-06-28T11:18:29+03:00"
+    },
+    {
+      "title": "Международный шахматный форум \"Moscow Open\"",
+      "is_main": false,
+      "dt_start": "2022-07-14T16:00:00+03:00",
+      "dt_end": "2022-07-20T20:00:00+03:00",
+      "dt_create": "2022-06-28T11:18:29+03:00"
+    },
+  ]
+  const [eventsSpisok, setEventsSpisok] = useState(array);
   const [error, setError] = useState(null);
+  
 
+  // useEffect(() => {
+  //   setInterval(() => {
+  //     fetch("https://beta.sosportom.ru/graphqdl/", {
+  //       method: "POST",
+  //       headers: {
+  //         "Content-type": "application/json",
+  //       },
+  //       body: JSON.stringify({
+  //         query:
+  //           "query videostandEvents ($videostand_id: ID!) { videostandEvents(videostand_id: $videostand_id) { current_and_upcoming { title, is_main, dt_start, dt_end, dt_create }, finished { title, is_main, dt_start, dt_end, dt_create } } }",
+  //         variables: { videostand_id: "6" },
+  //       }),
+  //     })
+  //       .then((res) => res.json())
+  //       .then((data) => {
+  //         setState(() => data.data.videostandEvents.current_and_upcoming);
+  //       })
+  //       .catch((err)=>{
+  //         setError(()=> err.toString())
+  //       });
+  //   }, 6000);
+  //   console.log(error)
 
-  useEffect(() => {
-    setInterval(() => {
-      fetch("https://beta.sosportom.ru/graphqdl/", {
-        method: "POST",
-        headers: {
-          "Content-type": "application/json",
-        },
-        body: JSON.stringify({
-          query:
-            "query videostandEvents ($videostand_id: ID!) { videostandEvents(videostand_id: $videostand_id) { current_and_upcoming { title, is_main, dt_start, dt_end, dt_create }, finished { title, is_main, dt_start, dt_end, dt_create } } }",
-          variables: { videostand_id: "6" },
-        }),
-      })
-        .then((res) => res.json())
-        .then((data) => {
-          setState(() => data.data.videostandEvents.current_and_upcoming);
-        })
-        .catch((err)=>{
-          setError(()=> err.toString())
-        });
-    }, 6000);
-    console.log(error)
+  // }, []);
 
-  }, []);
-
-  if(error !== null){
-    return(
-    <div className="App">
-      <div className="errorBox">
-      <h4 className="error">{error}</h4>
+  if (error !== null) {
+    return (
+      <div className="App">
+        <div className="errorBox">
+          <h4 className="error">{error}</h4>
+        </div>
       </div>
-    </div>
-    )
+    );
   }
   return (
     <div className="App">
-      {state.length ? <Event eventsSpisok={state} /> : <TimePage />}
+      {eventsSpisok.length ? <Event eventsSpisok={eventsSpisok} /> : <TimePage />}
     </div>
   );
 }
--- src/components/EventPage/EventContent.jsx
+++ src/components/EventPage/EventContent.jsx
@@ -1,14 +1,11 @@
 import { memo } from "react";
 import { useEffect } from "react";
 import { useState } from "react";
-import {
-  CircularProgressbarWithChildren,
-  buildStyles,
-} from "react-circular-progressbar";
 import "react-circular-progressbar/dist/styles.css";
 import { viewDate } from "../../modules/viewDate";
 import style from "./event.module.css";
 import GoesNow from "./GoesNow";
+import ProgressBar from "./ProgressBar/ProgressBar";
 
 const EventContent = ({ eventsSpisok }) => {
   const currenEventTime =
@@ -46,7 +43,12 @@ const EventContent = ({ eventsSpisok }) => {
   let secondProcent = (second / 60) * 100;
   let minuteProcent = (minute / 60) * 100;
   let hourProcent = (hour / 24) * 100;
-
+  let array = [
+    { type: "дней", time: day, procent: dayProcent },
+    { type: "часов", time: hour, procent: hourProcent },
+    { type: "минут", time: minute, procent: minuteProcent },
+    { type: "секунд", time: second, procent: secondProcent },
+  ];
   return (
     <div className={style.mainContent}>
       <div className={style.eventDay}>{viewDate(eventsSpisok)}</div>
@@ -55,132 +57,13 @@ const EventContent = ({ eventsSpisok }) => {
         <GoesNow />
       ) : (
         <div className={style.circularBlocks}>
-          <div className={style.circularBlock}>
-            <CircularProgressbarWithChildren
-              value={dayProcent}
-              styles={buildStyles({
-                rotation: 0.25,
-                strokeLinecap: "butt",
-                textSize: "50px",
-                pathTransitionDuration: 0.5,
-                // функция для определения цвета заполнения бара в зависимости от процента
-                pathColor: (function () {
-                  if (dayProcent >= 100) {
-                    return "#51ACD8";
-                  }
-                  if (dayProcent < 75 && dayProcent > 50) {
-                    return "orange";
-                  }
-                  if (dayProcent <= 50) {
-                    return "red";
-                  }
-                  return "#0062B5";
-                })(),
-                textColor: "#fff",
-                trailColor: "#fff",
-                backgroundColor: "#3e98c7",
-              })}
-            >
-              <div className={style.circleContent}>
-                <h3>{day}</h3>
-                <span>дней</span>
+          {array.map((item,ind) => {
+            return (
+              <div key={ind} className={style.circularBlock}>
+                <ProgressBar props={item} />
               </div>
-            </CircularProgressbarWithChildren>
-          </div>
-          <div className={style.circularBlock}>
-            <CircularProgressbarWithChildren
-              value={hourProcent}
-              styles={buildStyles({
-                rotation: 0.25,
-                strokeLinecap: "butt",
-                textSize: "50px",
-                pathTransitionDuration: 0.5,
-                pathColor: (function () {
-                  if (hourProcent >= 100) {
-                    return "#51ACD8";
-                  }
-                  if (hourProcent < 75 && hourProcent > 50) {
-                    return "orange";
-                  }
-                  if (hourProcent <= 50) {
-                    return "red";
-                  }
-                  return "#0062B5";
-                })(),
-                textColor: "#fff",
-                trailColor: "#fff",
-                backgroundColor: "#3e98c7",
-              })}
-            >
-              <div className={style.circleContent}>
-                <h3>{hour}</h3>
-                <span>часов</span>
-              </div>
-            </CircularProgressbarWithChildren>
-          </div>
-          <div className={style.circularBlock}>
-            <CircularProgressbarWithChildren
-              value={minuteProcent}
-              styles={buildStyles({
-                rotation: 0.25,
-                strokeLinecap: "butt",
-                textSize: "50px",
-                pathTransitionDuration: 0.5,
-                pathColor: (function () {
-                  if (minuteProcent >= 100) {
-                    return "#51ACD8";
-                  }
-                  if (minuteProcent < 75 && minuteProcent > 50) {
-                    return "orange";
-                  }
-                  if (minuteProcent <= 50) {
-                    return "red";
-                  }
-                  return "#0062B5";
-                })(),
-                textColor: "#fff",
-                trailColor: "#fff",
-                backgroundColor: "#3e98c7",
-              })}
-            >
-              <div className={style.circleContent}>
-                <h3>{minute}</h3>
-                <span>минут</span>
-              </div>
-            </CircularProgressbarWithChildren>
-          </div>
-          <div className={style.circularBlock}>
-            <CircularProgressbarWithChildren
-              value={secondProcent}
-              styles={buildStyles({
-                rotation: 0.25,
-                strokeLinecap: "butt",
-                textSize: "50px",
-                pathTransitionDuration: 0.5,
-                pathColor: (function () {
-                  if (secondProcent >= 100) {
-                    return "#51ACD8";
-                  }
-                  if (secondProcent < 75 && secondProcent > 50) {
-                    return "orange";
-                  }
-                  if (secondProcent <= 50) {
-                    return "red";
-                  }
-
-                  return "#0062B5";
-                })(),
-                textColor: "#fff",
-                trailColor: "#fff",
-                backgroundColor: "#3e98c7",
-              })}
-            >
-              <div className={style.circleContent}>
-                <h3>{second}</h3>
-                <span>секунд</span>
-              </div>
-            </CircularProgressbarWithChildren>
-          </div>
+            );
+          })}
         </div>
       )}
     </div>
